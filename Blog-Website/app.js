11d53d8144832debb1c6e5166988f038b89d4d50
yuzexia <xiayuze0406@gmail.com>
2023-03-22 11:41:03 +1100

    Setup Blog Web DataBase

--- Blog-Website/app.js
+++ Blog-Website/app.js
@@ -4,6 +4,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
+
+//mongoose connection
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -11,8 +14,17 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
-let posts = [];
 
+mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
+
+//Defining posts Schema
+const postSchema = {
+  title: String,
+  content: String
+};
+
+//Creating a model
+const Post = mongoose.model('Post', postSchema);
 
 app.use(express.static(__dirname, + "/public"));
 
@@ -22,14 +34,19 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
-app.get("/", function(req,res){
-  
-  res.render("home", {
-    homeContent:homeStartingContent,
-    posts: posts
-  });
-
+app.get("/", async function(req, res) {
+  try {
+    const posts = await Post.find({});
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts
+    });
+  } catch (err) {
+    console.error(err);
+  }
 });
+
+
 
 app.get("/about", function(req,res) {
   res.render("about",{
@@ -47,37 +64,39 @@ app.get("/compose",function(req,res){
   res.render("compose");
 });
 
-app.post("/compose", function(req,res) {
-   const post = {
-    "title": req.body.postTitle,
-    "content": req.body.postContent
-   }
-   posts.push(post);
+app.post("/compose", function(req,res){
+  const post = new Post ({
+    title: req.body.postTitle,
+    content: req.body.postContent
+  });
 
-   res.redirect("/");
+  post.save()
+    .then(()=>{
+      console.log("Post added to DB:)");
+      res.redirect("/");
+    })
+    .catch(err => {
+      res.status(400).send("Unale to save post to database.");
+    });
+
 });
 
 
 // route parameters
 
-app.get('/posts/:postName', (req,res)=> {
+app.get('/posts/:postId', (req,res)=> {
 
-  // res.send(req.postName);
-  const requestedTitle = _.lowerCase(req.params.postName);
+  //store postId parameter value
+  const requestedId = req.params.postId;
 
-  posts.forEach(function(post){
-    const storedTitle = _.lowerCase(post.title);
-
-    if(storedTitle === requestedTitle) {
-
+  Post.findById(requestedId)
+   .then( post => {
       res.render("post", {
         title: post.title,
         content: post.content
-
       })
-    };
-
-  });
+    })
+  
 });
 
 
