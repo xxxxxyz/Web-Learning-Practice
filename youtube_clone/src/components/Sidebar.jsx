5600af2d9b77deff310e032324741a4d6580032c
yuzexia <xiayuze0406@gmail.com>
2023-05-08 22:42:39 +1000

    Practice

---
+++ youtube_clone/src/components/Sidebar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Stack } from "@mui/material";
+
+import { categories } from "../utils/constants";
+
+const Sidebar = () => {
+  return <div>Sidebar</div>;
+};
+
+export default Sidebar;
